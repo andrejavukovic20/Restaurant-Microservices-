90b9374e20779ca64bc724e74f659abf8cb44a71
Andreja Vukovic <73890799+andrejavukovic20@users.noreply.github.com>
2025-08-16 17:18:03 +0200

    Delete restaurant-app directory

--- restaurant-app/src/main/java/restaurant/repositories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package restaurant.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import restaurant.models.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long>{
-
-}
